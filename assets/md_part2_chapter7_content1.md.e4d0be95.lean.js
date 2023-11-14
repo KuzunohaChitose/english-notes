d73640a42101aec0e2db20d601c300a8e1078321
Chitose <1595290233@qq.com>
2023-11-14 22:09:06 +0800

    ...

---
+++ assets/md_part2_chapter7_content1.md.e4d0be95.lean.js
@@ -0,0 +1,10 @@
+import{_ as e,C as s,o as a,c as i,k as t,H as n,Q as l}from"./chunks/framework.4871ade1.js";const w=JSON.parse('{"title":"强调句","description":"","frontmatter":{},"headers":[],"relativePath":"md/part2/chapter7/content1.md","filePath":"md/part2/chapter7/content1.md"}'),r={name:"md/part2/chapter7/content1.md"},c={class:"details custom-block"},p=t("summary",null,"Introduction",-1),h=l("",26);function d(g,m,u,b,k,_){const o=s("Mmd");return a(),i("div",null,[t("details",c,[p,n(o,{option:`graph LR;
+A((强调句)) --> B(句型);
+A --> C(注意点);
+B --> D[普通强调句];
+B --> E[疑问强调句];
+B --> F[特殊疑问强调句];
+B --> G[NotUntil强调句];
+B --> H[谓语强调句];
+C --> I[时态];
+C --> J[主谓一致];`})]),h])}const f=e(r,[["render",d]]);export{w as __pageData,f as default};
